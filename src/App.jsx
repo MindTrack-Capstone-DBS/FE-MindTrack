38d10678c2bd9ee4e6affff45619cb741958a35f
Georgio Armando Woda Kolo <georgioarmando8@gmail.com>
2025-05-28 03:39:50 +0700

    setup dan merge for react + vite

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,27 @@
-import LoginPage from './components/LoginPage';
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import Login from './pages/login/Login';
+import Register from './pages/login/Register';
+import Home from './pages/home/Home';
+import Profile from './pages/profile/Profile';
+import './App.css';
 
 function App() {
-  return <LoginPage />;
+  return (
+    <Router>
+      <div className="App">
+        <Routes>
+          {/* Redirect otomatis ke halaman Profile */}
+          <Route path="/" element={<Navigate to="/profile" />} />
+
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/home" element={<Home />} />
+          <Route path="/profile" element={<Profile />} />
+        </Routes>
+      </div>
+    </Router>
+  );
 }
 
 export default App;
