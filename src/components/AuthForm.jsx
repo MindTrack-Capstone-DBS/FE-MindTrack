65e4e2c4c902dce5ae25fae6bb0dc4cd2426e8c7
Georgio Armando Woda Kolo <georgioarmando8@gmail.com>
2025-06-06 16:10:50 +0700

    UI FIX login & regis

--- src/components/AuthForm.jsx
+++ src/components/AuthForm.jsx
@@ -29,7 +29,6 @@ function AuthForm({ isLogin = true }) {
     } else {
       // Register logic
       console.log('Register data:', formData);
-      // Redirect to login after successful registration
       navigate('/login');
     }
   };
@@ -39,52 +38,52 @@ function AuthForm({ isLogin = true }) {
   };
 
   return (
-    <div className="w-1/2 flex flex-col justify-center items-center p-8 bg-white">
-      <h2 className="text-xl mb-10 font-sans text-left w-full pl-4">
-        <b>MindTrack</b> - <span className="text-blue-700">Track Your Stress</span>
-      </h2>
-      <h1 className="text-4xl font-bold mb-8 text-center text-gray-900 font-sans">{isLogin ? 'Login' : 'Register'}</h1>
+    <div className="w-1/2 flex justify-center items-center bg-white overflow-x-hidden">
+      <div className="w-full max-w-sm">
+        <h2 className="text-xl mb-15 font-sans text-left absolute top-10 left-10">
+          <b>MindTrack</b> - <span className="text-blue-900 font-bold">Track Your Stress</span>
+        </h2>
+        <p className="text-4xl font-bold mb-10 mt-20 text-center text-gray font-sans">{isLogin ? 'Login' : 'Register'}</p>
 
-      <div className="w-full max-w-md mx-auto">
-        <form onSubmit={handleSubmit} className="flex flex-col gap-4">
+        <form onSubmit={handleSubmit} className="flex flex-col ">
           {!isLogin && (
             <>
-              <label className="text-lg font-medium text-white mb-1">Nama</label>
+              <label className="text-lg font-medium text-black ">Nama</label>
               <input
                 type="text"
                 name="name"
                 placeholder="Masukkan nama anda"
                 value={formData.name}
                 onChange={handleChange}
-                className="px-4 py-3 bg-white border-2 border-gray-200 rounded-lg text-white placeholder-gray-500 outline-none mb-3"
+                className="px-4 py-3 bg-white border-2 border-gray-200 rounded-lg text-black placeholder-gray-500 outline-none mb-3"
                 required
               />
 
-              <label className="text-lg font-medium text-white mb-1">No Telepon</label>
+              <label className="text-lg font-medium text-black mb-1">No Telepon</label>
               <input
                 type="text"
                 name="phone"
                 placeholder="Masukkan no telepon anda"
                 value={formData.phone}
                 onChange={handleChange}
-                className="px-4 py-3 bg-white border-2 border-gray-200 rounded-lg text-white placeholder-gray-500 outline-none mb-3"
+                className="px-4 py-3 bg-white border-2 border-gray-200 rounded-lg text-black placeholder-gray-500 outline-none mb-3"
                 required
               />
             </>
           )}
 
-          <label className="text-lg font-medium text-white mb-1">Email</label>
+          <label className="text-lg font-medium text-black mb-1">Email</label>
           <input
             type="email"
             name="email"
             placeholder="Masukkan email anda"
             value={formData.email}
             onChange={handleChange}
-            className="px-4 py-3 bg-white border-2 border-gray-200 rounded-lg text-white placeholder-gray-500 outline-none mb-3"
+            className="px-4 py-3 bg-white border-2 border-gray-200 rounded-lg text-black placeholder-gray-500 outline-none mb-3"
             required
           />
 
-          <label className="text-lg font-medium text-white mb-1">Password</label>
+          <label className="text-lg font-medium text-black mb-1">Password</label>
           <div className="relative flex items-center">
             <input
               type={showPassword ? 'text' : 'password'}
@@ -92,7 +91,7 @@ function AuthForm({ isLogin = true }) {
               placeholder="Masukkan Password anda"
               value={formData.password}
               onChange={handleChange}
-              className="w-full px-4 py-3 bg-white border-2 border-gray-200 rounded-lg text-white placeholder-gray-500 outline-none pr-10"
+              className="w-full px-4 py-3 bg-white border-2 border-gray-200 rounded-lg text-black placeholder-gray-500 outline-none pr-10"
               required
             />
             <button type="button" className="absolute right-3 text-gray-700" onClick={() => setShowPassword(!showPassword)}>
@@ -100,11 +99,11 @@ function AuthForm({ isLogin = true }) {
             </button>
           </div>
 
-          <button type="submit" className="bg-blue-700 text-white rounded-lg py-3.5 text-lg font-semibold mt-6 hover:bg-blue-800 transition-colors">
+          <button type="submit" className="bg-blue-900 text-white rounded-lg py-3.5 text-lg font-semibold mt-6 hover:bg-blue-500 transition-colors">
             {isLogin ? 'Login' : 'Register'}
           </button>
 
-          <div className="mt-6 text-center text-blue-400">
+          <div className="mt-6 text-center text-black">
             {isLogin ? 'Belum punya akun?' : 'Sudah punya akun?'}{' '}
             <span className="text-blue-700 font-semibold underline cursor-pointer ml-1" onClick={toggleAuthPage}>
               {isLogin ? 'Register' : 'Login'}
