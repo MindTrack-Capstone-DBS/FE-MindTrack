603ff276966bbadd6aa7c3c550ce3022be85723c
Georgio Armando Woda Kolo <georgioarmando8@gmail.com>
2025-06-16 18:12:54 +0700

    build: add production env and update vite config

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,13 @@ import tailwindcss from '@tailwindcss/vite';
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [tailwindcss(), react()],
+  build: {
+    outDir: 'dist',
+    sourcemap: false,
+  },
+  server: {
+    port: 3000,
+  },
   define: {
     'process.env': {},
   },
